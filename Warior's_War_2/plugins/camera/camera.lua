6bf53ee16e06bf500d6a1f76b6a8177023820a9d
philippe117 <philippe117@live.fr>
2017-12-26 21:40:42 -0500

    ajuste la camera

--- Warior's_War_2/plugins/camera/camera.lua
+++ Warior's_War_2/plugins/camera/camera.lua
@@ -8,15 +8,19 @@
 
 camera = {}
 camera.keyBinds = {up={"up", "w"}, down={"down", "s"}, left={"left", "a"}, right={"right", "d"}}
-camera.maxVelocity = {x=1, y=-1, z=1, zoom=1}
+camera.maxVelocity = {x=5, y=-5, z=1, zoom=1}
 camera.position = {x=0, y=0, z=1000, zoom=1}
-camera.velocity = {x=0, y=0, z=1000, zoom=0}
-camera.acceleration = {x=0, y=1000, z=0, zoom=0}
+camera.velocity = {x=0, y=0, z=0, zoom=0}
+camera.acceleration = {x=0, y=0, z=0, zoom=0}
 
 camera.elements = {}
 camera.elements.__mode = 'v'
+camera.mouseInScreen = {x, y}
+camera.mouseInWorld = {x, y, z, s, r}
 
 function plugin.update(dt)
+    camera.mouseInScreen.x, camera.mouseInScreen.y = love.mouse.getPosition()
+    camera.mouseInWorld.x, camera.mouseInWorld.y,  camera.mouseInWorld.z, camera.mouseInWorld.s = camera.screenToWorld(camera.mouseInScreen)
 
     camera.position.x = camera.position.x+camera.velocity.x
     camera.position.y = camera.position.y+camera.velocity.y
@@ -25,7 +29,6 @@ function plugin.update(dt)
 end
 
 function plugin.keypressed(key)
-    print("keypressed")
     if keyBinds.checkKeybind(key, camera.keyBinds.up) then
         camera.velocity.y = camera.velocity.y+camera.maxVelocity.y
     end
@@ -41,7 +44,6 @@ function plugin.keypressed(key)
 end
 
 function plugin.keyreleased(key)
-    print("keyreleased")
     if keyBinds.checkKeybind(key, camera.keyBinds.up) then
         camera.velocity.y = camera.velocity.y-camera.maxVelocity.y
     end
@@ -63,22 +65,31 @@ function camera.addElement(element)
     table.insert(camera.elements, element)
 end
 
-function camera.getScreenPosition(position)
-    local x = position.x-camera.position.x
-    local y = position.y-camera.position.y
+function camera.worldToScreen(position)
+    local s = math.abs(1000/(camera.position.z-position.z))
+    local x = (position.x-camera.position.x)*s*camera.position.zoom+love.graphics.getWidth()/2
+    local y = (position.y-camera.position.y)*s*camera.position.zoom+love.graphics.getHeight()/2
     local r = 0
-    local s = 1
     return x, y, r, s
+end
+
+function camera.screenToWorld(position, z)
+    if not z then z = 0 end
+    local s = math.abs(1000/(camera.position.z-z))
+    local x = (position.x-love.graphics.getWidth()/2)/camera.position.zoom/s+camera.position.x
+    local y = (position.y-love.graphics.getHeight()/2)/camera.position.zoom/s+camera.position.y
+    local r = 0
+    return x, y, z, r, 1/s
 end
 
 function camera.draw()
     for i, element in pairs(camera.elements) do
         if element.draw then
-            local x, y, r, s = camera.getScreenPosition(element.position)
+            local x, y, r, s = camera.worldToScreen(element.position)
+--            print(x)
             element.draw(x, y, r, s)
         end
     end
-    love.graphics.print("position=("..camera.position.x..", "..camera.position.x..")", 400, 00)
-    love.graphics.print("velocity=("..camera.velocity.x..", "..camera.velocity.x..")", 400, 10)
-
+    love.graphics.print("position=("..camera.position.x..", "..camera.position.y..")", 400, 00)
+    love.graphics.print("velocity=("..camera.velocity.x..", "..camera.velocity.y..")", 400, 10)
 end
--- Warior's_War_2/plugins/scriptManager/scriptManager.lua
+++ Warior's_War_2/plugins/scriptManager/scriptManager.lua
@@ -6,12 +6,12 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-    print("loading scripts...")
-    local scriptsNames = love.filesystem.getDirectoryItems("scripts")
-    for i, scriptName in pairs(scriptsNames) do
-        print("loading "..scriptName.."")
-        local filePath = "scripts/"..scriptName..""
-        if love.filesystem.exists(filePath) then
-            love.filesystem.load(filePath)()
-        end
+print("loading scripts...")
+local scriptsNames = love.filesystem.getDirectoryItems("scripts")
+for i, scriptName in pairs(scriptsNames) do
+    print("loading "..scriptName.."")
+    local filePath = "scripts/"..scriptName..""
+    if love.filesystem.exists(filePath) then
+        love.filesystem.load(filePath)()
     end
+end
