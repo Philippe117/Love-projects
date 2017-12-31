fc0b61ca027727cb9faa6b899ed5dc488f5eda29
philippe117 <philippe117@live.fr>
2017-12-31 17:08:56 -0500

    les roches tirent des roches asteur

--- Warior's_War_2/assets/objects/rock/rock.lua
+++ Warior's_War_2/assets/objects/rock/rock.lua
@@ -9,11 +9,10 @@
 rock = {}
 rock.__index = rock
 local path = "assets/objects/rock/"
-rock.range = 500
+rock.range = 4000
 
 function object.load()
     rock.image = love.graphics.newImage(""..path.."bule.png")
-    rock.cursor = {image = love.graphics.newImage(""..path.."shine.png")}
 end
 
 function rock.create(x, y, z, map, foes)
@@ -22,28 +21,42 @@ function rock.create(x, y, z, map, foes)
     self.velocity = {x=0, y=0, z=0 }
     self.foes = foes
     self.map = map
-    self.cursor = {position={x=x, y=y, z=z-1} }
-    self.cursor.draw = rock.drawcursor
-    self.cursor.image = rock.cursor.image
     camera.addElement(self)
-    camera.addElement(self.cursor)
     objectManager.subscribeObject(self)
     return self
 end
 
 function rock.update(self, dt)
-    self.velocity.y = self.velocity.y + 10*9.81*dt^2
-    self.position.x = self.position.x+self.velocity.x
-    self.position.y = self.position.y+self.velocity.y
-    self.position.z = self.position.z+self.velocity.z
+    self.velocity.y = self.velocity.y + 9.81*time.desiredRate*physicConstants.pixelPerMeter
+    self.position.x = self.position.x+self.velocity.x*time.desiredRate
+    self.position.y = self.position.y+self.velocity.y*time.desiredRate
+    self.position.z = self.position.z+self.velocity.z*time.desiredRate
 
     local angle = self.map:sphericalCollision(self.position.x, self.position.y, 15, 20)
     if angle ~= -10 then
 --        self.velocity.x = self.velocity.x+(0-self.velocity.x)*0.9
         self.position.x = self.position.x-math.cos(angle)*0.1
         self.position.y = self.position.y-math.sin(angle)*0.1
-        self.velocity.x, self.velocity.y = customPhysic.rebound(self.velocity.x, self.velocity.y, angle, 0, 0)
+        self.velocity.x, self.velocity.y = customPhysic.rebound(self.velocity.x, self.velocity.y, angle, 0.5, 0)
 --        map:damageMaterial(self.position.x, self.position.y, 30, 10, 0.5)
+    end
+    if self.target then
+        local dx = self.target.position.x-self.position.x
+        local dy = self.target.position.y-self.position.y
+        local dz = self.target.position.z-self.position.z
+        local distance = ((dx)^2+ (dy)^2+(dz)^2)^0.5
+        if distance < self.range then
+            local projectile = smallRock.create(self.position.x, self.position.y, self.position.z, self.map, self.foes)
+            projectile.velocity.x,
+            projectile.velocity.y,
+            projectile.velocity.z = aiTools.calculateFireSolution(
+                    self, self.target, {x=0, y=9.81*time.desiredRate*physicConstants.pixelPerMeter, z=0}, distance/5000*physicConstants.pixelPerMeter)
+        else
+            self.target = nil
+        end
+    end
+    if self.position.y > 2000 then
+        self.status = "dead"
     end
 end
 
@@ -62,8 +75,6 @@ function rock.slowUpdate(self, dt)
                     if sight then
                         targetDistance = distance
                         self.target = foe
-                        self.cursor.position.x = self.target.position.x
-                        self.cursor.position.y = self.target.position.y
                     end
                 end
             end
@@ -72,9 +83,5 @@ function rock.slowUpdate(self, dt)
 end
 
 function rock.draw(self, x, y, r, s)
-    love.graphics.draw(self.image ,x , y, r, s, s, self.image:getHeight()/2, self.image:getWidth()/2)
-end
-
-function rock.drawcursor(self, x, y, r, s)
     love.graphics.draw(self.image ,x , y, r, s, s, self.image:getHeight()/2, self.image:getWidth()/2)
 end
