dc28108bed0a2dd6b0feba51af90aab928fd839e
philippe117 <philippe117@live.fr>
2017-12-31 17:00:43 -0500

    ajoute une constante de dimention physique

---
+++ Warior's_War_2/scripts/physicConstants.lua
@@ -0,0 +1,10 @@
+--
+-- Created by Philippe La Madeleine
+-- User: philippe
+-- Date: 31/12/17
+-- Time: 2:55 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+physicConstants = {}
+physicConstants.pixelPerMeter = 15
