12be65416ffb2e44ad712cb119940393cbd6cdeb
philippe117 <philippe117@live.fr>
2018-01-03 13:12:18 -0500

    ajustement de la formule

--- Warior's_War_2/scripts/aiTools.lua
+++ Warior's_War_2/scripts/aiTools.lua
@@ -12,9 +12,9 @@ function aiTools.calculateFireSolution(origin, target, gravity, timeOfContact)
     local t = timeOfContact
     if not t then t = 0.1 end
     -- 30 is a mistery
-    local vx=(t*(t*-30*gravity.x+target.velocity.x)-origin.position.x+target.position.x)/t
-    local vy=(t*(t*-30*gravity.y+target.velocity.y)-origin.position.y+target.position.y)/t
-    local vz=(t*(t*-30*gravity.z+target.velocity.z)-origin.position.z+target.position.z)/t
+    local vx=(t*(t*-gravity.x/2+target.velocity.x)-origin.position.x+target.position.x)/t
+    local vy=(t*(t*-gravity.y/2+target.velocity.y)-origin.position.y+target.position.y)/t
+    local vz=(t*(t*-gravity.z/2+target.velocity.z)-origin.position.z+target.position.z)/t
 
     return vx, vy, vz
 end
